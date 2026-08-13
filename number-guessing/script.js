200a4b81b52de04b91fc9c879c9e4236a5fd7864
aayushshetty15 <aayushshetty1015@gmail.com>
2026-08-13 16:47:36 +0530

    feat: Build number guessing game with high low and close hints

--- number-guessing/script.js
+++ number-guessing/script.js
@@ -3,17 +3,28 @@ let result=document.getElementById("result");
 let attempts=0;
 let guessbtn=document.getElementById("guessBtn");
 let attemptsDisplay=document.getElementById("attempts")
+let resBtn=document.getElementById("resBtn");
 guessbtn.addEventListener("click",function(){
     let guess=Number(document.getElementById("num").value);
     attempts++;
     attemptsDisplay.textContent="Attempts: " + attempts;
-    if(guess>randomNumber){
-        result.textContent="Too high!";
+    if(guess===randomNumber){
+        result.textContent="Match Found"
+    }
+    else if(Math.abs(guess-randomNumber)<=5){
+        result.textContent="Very close!";
     }
     else if(guess<randomNumber){
         result.textContent="Too low!";
     }
     else{
-        result.textContent="Match Found";
+        result.textContent="Too high!"
     }
 });
+resBtn.addEventListener("click",function(){
+    randomNumber=Math.floor(Math.random()*100)+1;
+    attempts=0;
+    result.textContent=" ";
+    attemptsDisplay.textContent=" ";
+    document.getElementById("num").value="";
+})
